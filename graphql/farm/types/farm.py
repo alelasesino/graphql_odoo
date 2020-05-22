2710b159ce11ffac05ba2a84d0284cc449392ee0
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-22 20:33:01 +0200

    Create Farm Type

---
+++ graphql/farm/types/farm.py
@@ -0,0 +1,16 @@
+from graphene import ObjectType, String, Int, DateTime, List, Float
+from ..types.parcel import Parcel 
+
+
+class Farm(ObjectType):
+    id = Int()
+    name = String()
+    create_date = DateTime()
+    description = String()
+    code = String()
+    partner_id = Int()
+    parcels = List(Parcel)
+
+    @staticmethod
+    def resolve_parcels(root, info):
+        return root.parcel_ids or None
