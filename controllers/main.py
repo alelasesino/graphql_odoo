0a60c6d30ee7117eeee774be7dd68db170f74db7
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-22 17:19:48 +0200

    Update Controllers

---
+++ controllers/main.py
@@ -0,0 +1,24 @@
+from odoo import http
+from odoo.addons.graphql_base import GraphQLControllerMixin
+from ..schema import schema
+
+
+class GraphQLController(http.Controller, GraphQLControllerMixin):
+
+    # The GraphiQL route, providing an IDE for developers
+    @http.route("/graphiql", auth="user")
+    def graphiql(self, **kwargs):
+        return self._handle_graphiql_request(schema)
+
+    # Optional monkey patch, needed to accept application/json GraphQL
+    # requests. If you only need to accept GET requests or POST
+    # with application/x-www-form-urlencoded content,
+    # this is not necessary.
+    GraphQLControllerMixin.patch_for_json("^/graphql/?$")
+
+    # The graphql route, for applications.
+    # Note csrf=False: you may want to apply extra security
+    # (such as origin restrictions) to this route.
+    @http.route("/graphql", auth="user", csrf=False)
+    def graphql(self, **kwargs):
+        return self._handle_graphql_request(schema)
