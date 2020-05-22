92bc46ebe4228b07eac075e8a6a3dfe4bf0e962a
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-22 21:36:20 +0200

    Created Products Query

---
+++ graphql/product/queries/products.py
@@ -0,0 +1,11 @@
+from graphene import List
+from ... import FieldResolver
+from ..types.product import Product
+
+
+class ProductsQuery(FieldResolver):
+
+    Output = List(Product)
+
+    def resolve(self, info):
+        return info.context["env"]["product.product"].search([])
