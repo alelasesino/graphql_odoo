a1931ac6585359b434b30c0bff702d227c653f0c
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-23 20:00:58 +0200

    Update Receptions Query

--- graphql/reception/queries/receptions.py
+++ graphql/reception/queries/receptions.py
@@ -1,11 +1,28 @@
-from graphene import List
+import datetime
+from graphene import List, Boolean
 from ... import FieldResolver
 from ..types.reception import Reception
+from ...utils import utc_to_local
 
 
 class ReceptionsQuery(FieldResolver):
+    class Arguments:
+        today = Boolean(default_value=True)
 
     Output = List(Reception)
 
-    def resolve(self, info):
-        return info.context["env"]["stock.picking"].search([])
+    def resolve(self, info, today):
+
+        all_receptions = info.context["env"]["stock.picking"].search([])
+        
+        if today:
+            receptions = []
+            for reception in all_receptions:
+                scheduled_date=utc_to_local(reception.scheduled_date)
+
+                if datetime.datetime.today().day == scheduled_date.day:
+                    receptions.append(reception)
+
+            return receptions
+
+        return all_receptions
