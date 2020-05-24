dd818175a42c11634ff1f751d58cc65e2df08797
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-24 20:04:31 +0200

    Update Receptions Query

--- graphql/reception/queries/receptions.py
+++ graphql/reception/queries/receptions.py
@@ -1,4 +1,4 @@
-from graphene import List, Boolean
+from graphene import List, Boolean, String
 from ... import FieldResolver
 from ..types.reception import Reception
 from ...utils import utc_to_local, today_datetime_start, today_datetime_end
@@ -7,14 +7,19 @@ from ...utils import utc_to_local, today_datetime_start, today_datetime_end
 class ReceptionsQuery(FieldResolver):
     class Arguments:
         today = Boolean(default_value=True)
+        lot = String()
     
     Output = List(Reception)
 
-    def resolve(self, info, today):
+    def resolve(self, info, today, lot=None):
         domain = []
-        if today:
-            domain.append(('scheduled_date', '>=', today_datetime_start()))
-            domain.append(('scheduled_date', '<=', today_datetime_end()))
 
-        return info.context["env"]["stock.picking"].search(domain)
-        
+        if lot:
+            info.context["args"] = {'lot_filter': lot}
+            domain.append(('lot_id.name', '=', lot))
+
+        if today:
+            domain.append(('picking_id.scheduled_date', '>=', today_datetime_start()))
+            domain.append(('picking_id.scheduled_date', '<=', today_datetime_end()))
+
+        return info.context["env"]["stock.move.line"].search(domain).picking_id     
