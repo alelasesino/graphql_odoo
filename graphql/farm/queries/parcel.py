58b5724536527b7754fec5b6b59464badd3c5a91
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-05-27 21:21:21 +0200

    Added Error Not Exist

--- graphql/farm/queries/parcel.py
+++ graphql/farm/queries/parcel.py
@@ -1,6 +1,8 @@
 from graphene import Int
 from ... import FieldResolver
 from ..types.parcel import Parcel
+from odoo.exceptions import UserError
+from odoo import _
 
 
 class ParcelQuery(FieldResolver):
@@ -10,4 +12,7 @@ class ParcelQuery(FieldResolver):
     Output = Parcel
 
     def resolve(self, info, id):
-        return info.context["env"]["agro.farm.parcel"].search([('id', '=', id)]) or None
+        parcel = info.context["env"]["agro.farm.parcel"].search([('id', '=', id)])
+        if parcel:
+            return parcel
+        raise UserError(_(f"Parcel with ID: {id} not exist!"))
