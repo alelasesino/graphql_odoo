bd83701e79b1060f48363a7e560eef577e48c9a3
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-06-03 19:32:16 +0200

    Update Reception Types

--- graphql/reception/types/reception.py
+++ graphql/reception/types/reception.py
@@ -1,5 +1,5 @@
 from odoo.addons.graphql_base import OdooObjectType
-from graphene import InputObjectType, String, Int, DateTime, List
+from graphene import InputObjectType, String, Int, Float, DateTime, List
 from .product_reception import ProductReception
 from ...product.types.product import InputProduct
 
@@ -28,6 +28,30 @@ class Reception(OdooObjectType):
         return filter(lambda product: product.lot_id.name == info.context["args"]["lot_filter"], products)
 
 
+class ProductReception(OdooObjectType):
+    id = Int()
+    code = String()
+    display_name = String()
+    kilos = Float()
+    lote = String()
+
+    @staticmethod
+    def resolve_id(root, info):
+        return root.product_id.id
+
+    @staticmethod
+    def resolve_code(root, info):
+        return root.product_id.code
+
+    @staticmethod
+    def resolve_kilos(root, info):
+        return root.qty_done
+
+    @staticmethod
+    def resolve_lote(root, info):
+        return root.lot_id.name
+
+
 class InputReception(InputObjectType):
-    farm_id = Int()
+    farm_id = Int(required=True)
     products = List(InputProduct)
