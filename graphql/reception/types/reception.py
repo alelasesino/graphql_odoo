86978b741cc5097014535a281870df164b9c00e4
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-06-03 19:34:25 +0200

    Update Reception Types

--- graphql/reception/types/reception.py
+++ graphql/reception/types/reception.py
@@ -1,31 +1,6 @@
 from odoo.addons.graphql_base import OdooObjectType
 from graphene import InputObjectType, String, Int, Float, DateTime, List
-from .product_reception import ProductReception
 from ...product.types.product import InputProduct
-
-
-class Reception(OdooObjectType):
-    id = Int()
-    display_name = String()
-    scheduled_date = DateTime()
-    receive_from = Int()
-    time = String()
-    receive_products = List(ProductReception)
-
-    @staticmethod
-    def resolve_receive_from(root, info):
-        return root.location_id.id
-
-    @staticmethod
-    def resolve_time(root, info):
-        return root.scheduled_date.strftime("%H:%M")
-
-    @staticmethod
-    def resolve_receive_products(root, info):
-        products = root.move_ids_without_package.move_line_nosuggest_ids
-        if not 'args' in info.context:
-            return products
-        return filter(lambda product: product.lot_id.name == info.context["args"]["lot_filter"], products)
 
 
 class ProductReception(OdooObjectType):
@@ -50,6 +25,30 @@ class ProductReception(OdooObjectType):
     @staticmethod
     def resolve_lote(root, info):
         return root.lot_id.name
+
+
+class Reception(OdooObjectType):
+    id = Int()
+    display_name = String()
+    scheduled_date = DateTime()
+    receive_from = Int()
+    time = String()
+    receive_products = List(ProductReception)
+
+    @staticmethod
+    def resolve_receive_from(root, info):
+        return root.location_id.id
+
+    @staticmethod
+    def resolve_time(root, info):
+        return root.scheduled_date.strftime("%H:%M")
+
+    @staticmethod
+    def resolve_receive_products(root, info):
+        products = root.move_ids_without_package.move_line_nosuggest_ids
+        if not 'args' in info.context:
+            return products
+        return filter(lambda product: product.lot_id.name == info.context["args"]["lot_filter"], products)
 
 
 class InputReception(InputObjectType):
