65a701b0ba71211fe360eecadb42bbb90f17c603
Alejandro <alejperez99@hotmail.com>
2020-09-11 17:16:30 +0200

    [UPDATE] GraphQL Schema

--- graphql/schema.py
+++ graphql/schema.py
@@ -1,43 +1,12 @@
-from graphene import Schema, ObjectType 
-from odoo import _
-from odoo.addons.graphql_base import OdooObjectType
-
-from .farm.queries.farm import FarmQuery
-from .farm.queries.farms import FarmsQuery
-from .farm.mutations.farm import CreateFarm, RemoveFarm
-
-from .farm.queries.parcel import ParcelQuery
-from .farm.queries.parcels import ParcelsQuery
-
-from .reception.queries.reception import ReceptionQuery
-from .reception.queries.receptions import ReceptionsQuery
-from .reception.mutations.reception import CreateReception
-
-from .product.queries.product import ProductQuery
-from .product.queries.products import ProductsQuery
+from graphene import Schema, ObjectType
 
 
 class Query(ObjectType):
-
-    farms = FarmsQuery.Field()
-    farm = FarmQuery.Field()
-
-    parcels = ParcelsQuery.Field()
-    parcel = ParcelQuery.Field()
-
-    receptions = ReceptionsQuery.Field()
-    reception = ReceptionQuery.Field()
-
-    products = ProductsQuery.Field()
-    product = ProductQuery.Field()
+    pass
 
 
 class Mutation(ObjectType):
-    
-    create_farm = CreateFarm.Field()
-    remove_farm = RemoveFarm.Field()
-
-    create_reception = CreateReception.Field()
+    pass
 
 
 schema = Schema(query=Query, mutation=Mutation)
