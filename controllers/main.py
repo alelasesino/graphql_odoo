bb6f7ca0e3f331f42156673e33084750f1cbda1c
Alejandro <alejperez99@hotmail.com>
2020-12-04 14:10:28 +0100

    [UPDATE] Main Controller

--- controllers/main.py
+++ controllers/main.py
@@ -3,7 +3,7 @@ from odoo.addons.graphql_base import GraphQLControllerMixin
 from ..graphql.schema import obtain_schema
 
 
-GRAPHQL_MODULES = ["graphql_stock", "graphql_mrp", "graphql_sale"]
+GRAPHQL_MODULES = ["graphql_stock", "graphql_mrp", "graphql_sale", "graphql_purchase"]
 
 
 class GraphQLController(Controller, GraphQLControllerMixin):
