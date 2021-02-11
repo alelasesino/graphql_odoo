902ae84f2e7419844ea095866e19828e95e3fb27
Alejandro <alejperez99@hotmail.com>
2021-02-11 21:29:25 +0100

    [UPDATE] Product Model Inherit

--- models/product.py
+++ models/product.py
@@ -1,8 +1,14 @@
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     colour = fields.Char(string="Color")
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    priority = fields.Integer(string="Prioridad")
